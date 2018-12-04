f3ec7b61c23852220b6ea7d75d239c8f4fe42b0f
sherzinger <sascha.herzinger@uni.lu>
2018-12-04 10:27:46 +0100

    improved sorting

--- src/charts/impl/Heatmap.js
+++ src/charts/impl/Heatmap.js
@@ -10,8 +10,8 @@ const margin = {
   bottom: 50,
   left: 50,
 };
-const width = 500 - margin.left - margin.right;
-const height = 500 - margin.top - margin.bottom;
+const width = 1000 - margin.left - margin.right;
+const height = 1000 - margin.top - margin.bottom;
 
 export default class extends Chart {
   constructor({ container }) {
@@ -29,35 +29,53 @@ export default class extends Chart {
       .attr('height', height + margin.top + margin.bottom)
       .append('g')
       .attr('transform', `translate(${margin.left}, ${margin.top})`);
+    this.data = [];
   }
 
   static get name() {
     return 'heatmap';
   }
 
-  update({ data, valueRange, title, rowNames, colNames, sequential }) {
-    this.databind(data, valueRange, rowNames, colNames, sequential);
-    const timer = d3.timer((elapsed) => {
-      this.draw(this.canvas);
-      if (elapsed > 5000) timer.stop();
+  prepareData({ values, rows, cols }) {
+    this.data = [];
+    rows.forEach((row, i) => {
+      cols.forEach((col, j) => {
+        this.data.push({ value: values[i * rows.length + j], row, col });
+      });
     });
   }
 
-  databind(data, valueRange, rowNames, colNames, sequential) {
-    function getMatrixIndex(row, col) { return row * colNames.length + col; }
-    const rectSize = Math.floor(height / rowNames.length); // FIXME: this is just a placeholder
+  update({ values, valueRange, title, rows, cols, sequential }) {
+    if (typeof values !== 'undefined') {
+      this.prepareData({ values, rows, cols });
+    }
+
+    this.databind({ data: this.data, rows, cols, valueRange, sequential });
+    const timer = d3.timer((elapsed) => {
+      this.draw(this.canvas);
+      if (elapsed > ANIMATION_DURATION) timer.stop();
+    });
+  }
+
+  databind({ rows, cols, valueRange, sequential }) {
+    const rowMapping = {};
+    const colMapping = {};
+    rows.forEach((d, i) => { rowMapping[d] = i; });
+    cols.forEach((d, i) => { colMapping[d] = i; });
+
+    const rectSize = Math.floor(height / rows.length); // FIXME: this is just a placeholder
 
     let colorScale = null;
     if (sequential) {
       colorScale = d3.scaleSequential(d3.interpolateRdBu)
-        .domain(valueRange || d3.extent(data));
+        .domain(valueRange);
     } else {
       colorScale = d3.scaleSequential(d3.interpolateBlues)
-        .domain(valueRange || d3.extent(data));
+        .domain(valueRange);
     }
 
     const rect = this.memory.selectAll('rect')
-      .data(data);
+      .data(this.data, d => `${d.row}-${d.col}`);
 
     rect.enter()
       .append('rect')
@@ -66,15 +84,15 @@ export default class extends Chart {
       .duration(ANIMATION_DURATION)
       .attr('width', rectSize)
       .attr('height', rectSize)
-      .attr('x', (d, i) => (i % colNames.length) * rectSize)
-      .attr('y', (d, i) => Math.floor(i / colNames.length) * rectSize)
-      .attr('fillStyle', d => colorScale(d));
+      .attr('x', d => colMapping[d.col] * rectSize)
+      .attr('y', d => rowMapping[d.row] * rectSize)
+      .attr('fillStyle', d => colorScale(d.value));
 
     rect.exit()
       .remove();
 
     const rowLabels = this.svg.selectAll('text.row-label')
-      .data(rowNames);
+      .data(rows, d => d);
 
     rowLabels.enter()
       .append('text')
@@ -82,28 +100,18 @@ export default class extends Chart {
       .attr('text-anchor', 'end')
       .style('font-size', '10px') // FIXME: needs to be dynamic
       .merge(rowLabels)
-      .attr('transform', (d, i) => `translate(0, ${(i + 0.5) * rectSize})rotate(-45)`)
+      .attr('transform', d => `translate(0, ${(rowMapping[d] + 0.5) * rectSize})rotate(-45)`)
       .text(d => d)
-      .on('click', (_, rowIdx) => {
-        const sortValues = data.slice(rowIdx * colNames.length, (rowIdx + 1) * colNames.length);
-        const sortOrder = d3.range(0, colNames.length, 1)
-          .sort((k, l) => {
-            if (sortValues[k] < sortValues[l]) return -1;
-            if (sortValues[k] > sortValues[l]) return 1;
-            return 0;
-          });
-        const newData = [];
-        for (let i = 0; i < rowNames.length; i += 1) {
-          sortOrder.forEach((sortIdx) => {
-            newData.push(data[getMatrixIndex(i, sortIdx)]);
-          });
-        }
-        const newColNames = sortOrder.map(sortIdx => colNames[sortIdx]);
-        this.update({ data: newData, valueRange, rowNames, colNames: newColNames, sequential });
+      .on('click', (row) => {
+        const newCols = this.data
+          .filter(d => d.row === row)
+          .sort((a, b) => b.value - a.value)
+          .map(d => d.col);
+        this.update({ rows, cols: newCols, valueRange, sequential });
       });
 
     const colLabels = this.svg.selectAll('text.col-label')
-      .data(colNames);
+      .data(cols, d => d);
 
     colLabels.enter()
       .append('text')
@@ -111,24 +119,14 @@ export default class extends Chart {
       .attr('text-anchor', 'end')
       .style('font-size', '10px') // FIXME: needs to be dynamic
       .merge(colLabels)
-      .style('transform', (d, i) => `translate(${(i + 0.5) * rectSize}px, 0px)rotate(45deg)`)
+      .style('transform', d => `translate(${(colMapping[d] + 0.5) * rectSize}px, 0px)rotate(45deg)`)
       .text(d => d)
-      .on('click', (_, colIdx) => {
-        const sortValues = rowNames.map((__, i) => data[getMatrixIndex(i, colIdx)]);
-        const sortOrder = d3.range(0, rowNames.length, 1)
-          .sort((k, l) => {
-            if (sortValues[k] < sortValues[l]) return -1;
-            if (sortValues[k] > sortValues[l]) return 1;
-            return 0;
-          });
-        const newData = [];
-        sortOrder.forEach((sortIdx) => {
-          for (let i = 0; i < colNames.length; i += 1) {
-            newData.push(data[getMatrixIndex(sortIdx, i)]);
-          }
-        });
-        const newRowNames = sortOrder.map(sortIdx => rowNames[sortIdx]);
-        this.update({ data: newData, valueRange, rowNames: newRowNames, colNames, sequential });
+      .on('click', (col) => {
+        const newRows = this.data
+          .filter(d => d.col === col)
+          .sort((a, b) => b.value - a.value)
+          .map(d => d.row);
+        this.update({ rows: newRows, cols, valueRange, sequential });
       });
   }
 
